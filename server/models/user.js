b22d0165eb83f2848011b9ea7e061b50b0f6ef03
Neal Uehara <ueharaneal@gmail.com>
2023-12-16 18:23:33 -0800

    user models completed

--- server/models/user.js
+++ server/models/user.js
@@ -1,16 +1,55 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
+const validator = require("validator");
 
 const userSchema = mongoose.Schema({
-    email:{},
-    password:{},
-    role: {},
-    firstname: {},
-    lastname:{},
-    age:{},
-    date:{},
-    verified:{},
-})
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid Email");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true, 
+    trim: true,
+    lowercase: true,
+  },
+  role: {
+    type: String,
+    enum:['user','admin'],
+    default: 'user' 
+  },
+  firstname: {
+    type: String,
+    trim: true,
+    maxLength: 100,
+  },
+  lastname: {
+    type: String,
+    trim: true,
+    maxLength: 100,
+  },
+  age: {
+    type: Number,
+    trim: true,
+    maxLength: 100,
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-const User = mongoose.model('User', userSchema);
-module.exports = { User }
+const User = mongoose.model("User", userSchema);
+module.exports = { User };
