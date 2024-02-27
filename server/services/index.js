69c300d6db54018e432249736299959cd15ff77f
Neal Uehara <ueharaneal@gmail.com>
2024-02-26 22:25:49 -0800

    saved

--- server/services/index.js
+++
@@ -1,6 +0,0 @@
-
-
-module.exports.authService = require('./auth.service');
-module.exports.userService = require('./user.service')
-module.exports.articlesService = require('./articles.service')
-module.exports.emailService = require('./email.service')
