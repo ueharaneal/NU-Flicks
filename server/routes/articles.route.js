f669be33a3dde3302a88ebe4e84c72ceb9b0823b
Neal Uehara <ueharaneal@gmail.com>
2024-03-09 13:55:34 -0800

    completed Articles validation for directors and title

--- server/routes/articles.route.js
+++ server/routes/articles.route.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const articlesController = require("../controllers/articles.controller");
+const { addArticleValidator } = require('../middleware/validation')
 
 const auth = require("../middleware/auth");
 
 router.post(
   "/",
   auth("createAny", "articles"),
+  addArticleValidator,
   articlesController.createArticle
 );
 //categories
