f02181c9d9dc6996ed7d1be4590258e8b65e0e22
Neal Uehara <ueharaneal@gmail.com>
2024-03-23 14:48:06 -0700

    Created baseline header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-
-import Home from './pages/Home'
+import { ThemeProvider } from "./components/theme/theme-provider";
+import Home from "./pages/Home";
 
 import Header from "./components/navigation/Header";
 function App() {
   return (
-    <BrowserRouter>
-    <Header/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route/>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- client/src/components/navigation/Header.tsx
+++ client/src/components/navigation/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -10,10 +11,16 @@ import {
 } from "@ui/navigation-menu";
 
 import { navigationMenuTriggerStyle } from "@components/ui/navigation-menu";
+import { ModeToggle } from "../theme/mode-toggle";
 
 const Header = () => {
   return (
-    <div className="flex flex-row px-4 m-4 justify-center border-x-4border-foreground">
+    <div className="flex flex-row px-4 m-4 justify-between border-x-4 border-foreground">
+      <Link to="/">
+        <h1 className="text-primary p-2 rounded-lg text-heavy text-4xl hover:bg-muted">
+          NU FL<span className="text-foreground">I</span>CKS
+        </h1>
+      </Link>
       <NavigationMenu className="">
         <NavigationMenuList>
           <NavigationMenuItem>
@@ -31,6 +38,9 @@ const Header = () => {
             <NavigationMenuContent>
               <NavigationMenuLink>Link2</NavigationMenuLink>
             </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <ModeToggle />
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
