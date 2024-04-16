8171814b8e50f248bcd00a3f9abe2585f3e725d3
Neal Uehara <ueharaneal@gmail.com>
2024-04-15 23:29:38 -0700

    completed

--- client/src/components/navigation/Header.tsx
+++ client/src/components/navigation/Header.tsx
@@ -4,12 +4,14 @@ import { RootState } from "@/store";
 import { useDispatch, useSelector } from "react-redux";
 import {setLayout} from '../../store/reducers/site'
 import {useEffect} from 'react'
+import { clearNotifications } from "@/store/reducers/notifications";
 //LOGGED OUT HEADER
 const Header = () => {
   const users = useSelector((state:RootState)=>state.users)
   const dispatch = useDispatch()
   const location = useLocation()
-
+  const notifications = useSelector((state:RootState)=>state.notifications)
+//This determines which window is going to be opened
   useEffect(()=>{
     let pathname = location.pathname.split('/')
     if(pathname[1] == "dashboard"){
@@ -18,6 +20,22 @@ const Header = () => {
       dispatch(setLayout(''))
      }
   },[location.pathname, dispatch])
+  //this determines the notifications in the header 
+  useEffect(()=>{
+    let {global} = notifications
+    if(notifications && global.error){
+        //show message 
+        const msg = global.msg ? global.msg : 'Error'
+        console.log(msg)
+        dispatch(clearNotifications())
+    }
+    if(notifications && global.success){
+      const msg = global.msg ? global.msg : 'Good!'
+      console.log(msg)
+      dispatch(clearNotifications())
+    }
+  },[notifications])
+
   return (
     <div className="fixed-top flex flex-row px-5 m-4 justify-between border-x-4 border-foreground">
       <Link to="/">
