071ca24dfcb35cc161038141f3c29b6ca981fcae
Neal Uehara <79432460+ueharaneal@users.noreply.github.com>
2024-05-03 00:04:47 -0700

    need to fix missing files problme

---
+++ client/src/components/Feed/Hero.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Categories from "./Categories"
+const Hero: React.FC = () => {
+	return (
+		<div>
+			Hero page
+			<Categories />
+		</div>
+	)
+}
+
+export default Hero
