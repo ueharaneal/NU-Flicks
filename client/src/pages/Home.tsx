76d08731dbe5a8e5d6e1aa61bf39a47e1fc79e82
Neal Uehara <79432460+ueharaneal@users.noreply.github.com>
2024-05-09 23:58:28 -0700

    feed

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,16 +1,17 @@
-import Categories from "@/components/Feed/Categories";
-import Hero from "@/components/Feed/Hero";
+import Categories from "@/components/Feed/Categories"
+import Hero from "@/components/Feed/Hero"
 function Home() {
-  return (
-    <div className="">
-      <div className=" flex flex-col items-center relative">
-        <Hero/>
-        <div className="absolute top-3/4 w-5/6 text-left flex ">
-          <Categories />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className=''>
+			<div className=' flex flex-col items-center relative'>
+				<Hero />
+				<div className='absolute top-3/4 w-5/6 text-left flex '>
+					<p>Feed </p>
+					<Categories />
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default Home;
+export default Home
