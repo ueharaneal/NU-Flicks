fc3dc3575649b9814652553fcdcfa2d8c8309f98
Neal Uehara <79432460+ueharaneal@users.noreply.github.com>
2024-05-24 23:58:02 -0700

    fixed error response

--- client/src/store/actions/users.ts
+++ client/src/store/actions/users.ts
@@ -1,88 +1,88 @@
-import { createAsyncThunk, AsyncThunkAction } from "@reduxjs/toolkit";
-import {errorGlobal, successGlobal} from '../reducers/notifications'
-import axios from "axios";
+import { createAsyncThunk, AsyncThunkAction } from "@reduxjs/toolkit"
+import { errorGlobal, successGlobal } from "../reducers/notifications"
+import axios from "axios"
 
-import { AppDispatch } from "../index";
-import { UserData } from "../reducers/users";
-import { getAuthHeader, removeTokenCookie } from "@/utils/tools";
-type ThunkAction = AsyncThunkAction<{ data: any; auth: boolean }, void, {}>;
+import { AppDispatch } from "../index"
+import { UserData } from "../reducers/users"
+import { getAuthHeader, removeTokenCookie } from "@/utils/tools"
+type ThunkAction = AsyncThunkAction<{ data: any; auth: boolean }, void, {}>
 
 interface RegisterUserArgs {
-  email: string;
-  password: string;
-  firstname: string | null;
-  lastname: string | null;
+	email: string
+	password: string
+	firstname: string | null
+	lastname: string | null
 }
 
 interface SignInArgs {
-  email: string;
-  password: string;
+	email: string
+	password: string
 }
 
 export const registerUser = createAsyncThunk<
-  { data: UserData; auth: boolean },
-  RegisterUserArgs,
-  { dispatch: AppDispatch }
+	{ data: UserData; auth: boolean },
+	RegisterUserArgs,
+	{ dispatch: AppDispatch }
 >(
-  "users/registerUser",
-  async ({ email, password, firstname, lastname }, { dispatch }) => {
-    try {
-      const request = await axios.post("/api/auth/register", {
-        email: email,
-        password: password,
-        firstname: firstname,
-        lastname: lastname,
-      });
-      //show a message
-      dispatch(successGlobal("Welcome! Check your email to validate! "))
-      return { data: request.data.user, auth: true };
-    } catch (error) {
-      dispatch(errorGlobal(error.response.data.message))
-      console.error(error);
-      throw error;
-    }
-  }
-);
+	"users/registerUser",
+	async ({ email, password, firstname, lastname }, { dispatch }) => {
+		try {
+			const request = await axios.post("/api/auth/register", {
+				email: email,
+				password: password,
+				firstname: firstname,
+				lastname: lastname,
+			})
+			//show a message
+			dispatch(successGlobal("Welcome! Check your email to validate! "))
+			return { data: request.data.user, auth: true }
+		} catch (error) {
+			dispatch(errorGlobal(error.response.data.message))
+			console.error(error)
+			throw error
+		}
+	}
+)
 
 export const signInUser = createAsyncThunk<
-  { data: UserData; auth: boolean },
-  SignInArgs,
-  { dispatch: AppDispatch }
+	{ data: UserData; auth: boolean },
+	SignInArgs,
+	{ dispatch: AppDispatch }
 >("users/signInUser", async ({ email, password }, { dispatch }) => {
-  try {
-    const request = await axios.post("/api/auth/signin", {
-      email,
-      password,
-    });
-    //show a message
-    console.log("itworked");
-    console.log(request);
-    dispatch(successGlobal("Welcome!"))
-    return { data: request.data.user, auth: true };
-  } catch (error) {
-    //gotta figure this error message out 
-    dispatch(errorGlobal(error.repsonse.data.message))
-    console.error(error);
-    throw error;
-  }
-});
+	try {
+		const request = await axios.post("/api/auth/signin", {
+			email,
+			password,
+		})
+		//show a message
+		console.log("itworked")
+		console.log(request)
+		dispatch(successGlobal("Welcome!"))
+		return { data: request.data.user, auth: true }
+	} catch (error) {
+		//gotta figure this error message out
+		dispatch(errorGlobal(error.response.data.message))
+		console.error(error)
+		throw error
+	}
+})
 
 export const isAuth = createAsyncThunk<{ data: any; auth: boolean }, void>(
-  "users/isAuth",
-  async () => {
-    try {
-      const request = await axios.get("/api/auth/isauth", getAuthHeader());
-      return { data: request.data, auth: true };
-    } catch (error) {
-      return { data: {}, auth: false };
-    }
-  }
-);
+	"users/isAuth",
+	async () => {
+		try {
+			const request = await axios.get("/api/auth/isauth", getAuthHeader())
+			return { data: request.data, auth: true }
+		} catch (error) {
+			return { data: {}, auth: false }
+		}
+	}
+)
 
 export const signOut = createAsyncThunk<{ data: any; auth: boolean }, void>(
-  "users/signout",
-  async () => {
-    removeTokenCookie();
-    return { data: null, auth: false };
-  }
-);
+	"users/signout",
+	async () => {
+		removeTokenCookie()
+		return { data: null, auth: false }
+	}
+)
